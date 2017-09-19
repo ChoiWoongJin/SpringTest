b5633d05118f6bab2d35bc90cfd2e54d5e18635b
ChoiWoongJin <musi0436@naver.com>
2017-09-20 02:59:58 +0900

    Not Spring Example

---
+++ src/main/java/spring/choi/test3/Calculator.java
@@ -0,0 +1,29 @@
+package spring.choi.test3;
+
+public class Calculator {
+	
+	public void addition(int f, int s) {
+		System.out.println("addition()");
+		int result = f + s;
+		System.out.println(f + " + " + s + " = " + result);
+	}
+	
+	public void subtraction(int f, int s) {
+		System.out.println("addition()");
+		int result = f - s;
+		System.out.println(f + " - " + s + " = " + result);
+	}
+	
+	public void multiplication(int f, int s) {
+		System.out.println("addition()");
+		int result = f * s;
+		System.out.println(f + " * " + s + " = " + result);
+	}
+	
+	public void division(int f, int s) {
+		System.out.println("addition()");
+		int result = f / s;
+		System.out.println(f + " / " + s + " = " + result);
+	}
+
+}
--- src/main/java/spring/choi/test3/MainClass.java
+++ src/main/java/spring/choi/test3/MainClass.java
@@ -1,12 +1,27 @@
 package spring.choi.test3;
 
+
+/**
+ * 스프링이 적용되지 않은 예
+ *
+ */
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		MyCalculator myCalculator = new MyCalculator();
+		// 직접 생성하는 방식은 좋지 않음
+		// MyCalculator가 변경되야 하는 경우 코드를 직접 변경해야 하는 경우 발생
+		myCalculator.setCalculator(new Calculator());
 		
-
+		myCalculator.setFirstNum(10);
+		myCalculator.setSecondNum(2);
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
 	}
 
 }
