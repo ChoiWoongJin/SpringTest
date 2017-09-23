28ca1b934889172dcf357e853dcf5788d0c01810
ChoiWoongJin <musi0436@naver.com>
2017-09-23 17:40:10 +0900

    SpringTest5

---
+++ src/main/java/spring/choi/test5/MainClass.java
@@ -0,0 +1,21 @@
+package spring.choi.test5;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		String con = "classpath:applicationCTX5.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(con);
+		StudentInfo studentInfo = ctx.getBean("studentInfo", StudentInfo.class);
+		studentInfo.getStudentInfo();
+		
+		Student student2 = ctx.getBean("student2", Student.class);
+		studentInfo.setStudent(student2);
+		studentInfo.getStudentInfo();
+		
+		ctx.close();
+	}
+
+}
