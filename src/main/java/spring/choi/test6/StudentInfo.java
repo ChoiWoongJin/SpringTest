6abbc04316fc5bf9660727c58d812bf5c13b3b1e
ChoiWoongJin <musi0436@naver.com>
2017-09-25 03:21:25 +0900

    SpringTest6

---
+++ src/main/java/spring/choi/test6/StudentInfo.java
@@ -0,0 +1,19 @@
+package spring.choi.test6;
+
+public class StudentInfo {
+	
+	private Student student;
+	
+	public StudentInfo() {
+		
+	}
+	
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
+	public Student getStudent() {
+		return student;
+	}
+
+}
